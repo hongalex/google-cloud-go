035e405fcad19a5acf51c99842e3f45b0fe0d565
Alex Hong <hongalex@google.com>
2019-07-29 14:52:23 -0700

    pubsub: refactor subscribe metrics

--- pubsub/trace.go
+++ pubsub/trace.go
@@ -15,10 +15,6 @@
 package pubsub
 
 import (
-	"context"
-	"log"
-	"sync"
-
 	"go.opencensus.io/plugin/ocgrpc"
 	"go.opencensus.io/stats"
 	"go.opencensus.io/stats/view"
@@ -51,7 +47,7 @@ const statsPrefix = "cloud.google.com/go/pubsub/"
 var (
 	// PublishedMessages is a measure of the number of messages published, which may include errors.
 	// It is EXPERIMENTAL and subject to change or removal without notice.
-	PublishedMessages = stats.Int64(statsPrefix+"published_messages", "Number of PubSub message published", stats.UnitDimensionless)
+	PublishedMessages = stats.Int64(statsPrefix+"published_messages", "Number of PubSub messages published", stats.UnitDimensionless)
 
 	// PublishLatency is a measure of the number of milliseconds it took to publish a bundle,
 	// which may consist of one or more messages.
@@ -141,6 +137,13 @@ var (
 	StreamResponseCountView *view.View
 )
 
+// Default views are arrays that contain all views related to publish/subscribe operations provided by this package.
+// It is EXPERIMENTAL and subject to change or removal without notice.
+var (
+	DefaultPublishViews   []*view.View
+	DefaultSubscribeViews []*view.View
+)
+
 func init() {
 	PublishedMessagesView = createCountView(stats.Measure(PublishedMessages), keyTopic, keyStatus, keyError)
 	PublishLatencyView = createDistView(PublishLatency, keyTopic, keyStatus, keyError)
@@ -172,13 +175,6 @@ func init() {
 	}
 }
 
-// The following arrays are the default views related to publish/subscribe operations provided by this package.
-// It is EXPERIMENTAL and subject to change or removal without notice.
-var (
-	DefaultPublishViews   []*view.View
-	DefaultSubscribeViews []*view.View
-)
-
 func createCountView(m stats.Measure, keys ...tag.Key) *view.View {
 	return &view.View{
 		Name:        m.Name(),
@@ -197,21 +193,4 @@ func createDistView(m stats.Measure, keys ...tag.Key) *view.View {
 		Measure:     m,
 		Aggregation: view.Distribution(0, 25, 50, 75, 100, 200, 400, 600, 800, 1000, 2000, 4000, 6000),
 	}
-}
-
-var logOnce sync.Once
-
-// withSubscriptionKey returns a new context modified with the subscriptionKey tag map.
-func withSubscriptionKey(ctx context.Context, subName string) context.Context {
-	ctx, err := tag.New(ctx, tag.Upsert(keySubscription, subName))
-	if err != nil {
-		logOnce.Do(func() {
-			log.Printf("pubsub: error creating tag map for 'subscribe' key: %v", err)
-		})
-	}
-	return ctx
-}
-
-func recordStat(ctx context.Context, m *stats.Int64Measure, n int64) {
-	stats.Record(ctx, m.M(n))
 }
